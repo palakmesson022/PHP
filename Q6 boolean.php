3fec1fe159d42d4e9545dde23267343f051a640b
palakmesson022 <palakmesson@gmail.com>
2025-01-30 12:18:53 +0530

    Update Q6 boolean.php

--- Q6 boolean.php
+++ Q6 boolean.php
@@ -2,5 +2,5 @@
     // Assign the value TRUE to a variable
     $show_error = true;
     var_dump($show_error);
-    echo("<br>This program is written by Prabhav Nasa<br>ERPID-0221BCA011");
+    echo("<br>This program is written by Palak Messon");
 ?>
