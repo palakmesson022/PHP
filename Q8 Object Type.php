52776a579749bc5805cbf28409be8f3dfa6d694a
palakmesson022 <palakmesson@gmail.com>
2025-01-30 12:19:16 +0530

    Update Q8 Object Type.php

--- Q8 Object Type.php
+++ Q8 Object Type.php
@@ -16,5 +16,5 @@
     // Create object from class
     $message = new greeting;
     var_dump($message);
-    echo("<br>This program is written by Prabhav Nasa<br>ERPID-0221BCA011");
+    echo("<br>This program is written by Palak Messon");
 ?>
