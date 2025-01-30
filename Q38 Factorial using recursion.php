b68177daa09d24bd258f005b28bd3293d6c59a12
palakmesson022 <palakmesson@gmail.com>
2025-01-30 12:17:57 +0530

    Update Q38 Factorial using recursion.php

--- Q38 Factorial using recursion.php
+++ Q38 Factorial using recursion.php
@@ -8,5 +8,5 @@ return $number*factorial($number-1);
 $number=5;
 $result=factorial($number);
 echo"Factorial of $number is $result";
-echo("<br>This program is written by Prabhav Nasa<br>ERPID-0221BCA011");
+echo("<br>This program is written by Palak Messon");
 ?>
