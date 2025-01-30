1f755871559c3328e3bdfdd56e97cedd108da148
palakmesson022 <palakmesson@gmail.com>
2025-01-30 12:15:49 +0530

    Update Q28 Sqaure of Number.php

--- Q28 Sqaure of Number.php
+++ Q28 Sqaure of Number.php
@@ -8,5 +8,5 @@ $square = $number * $number;
 // Output the result
 echo "The square of " . $number . " is: " . $square;
 
-echo("<br>This program is written by Prabhav Nasa<br>ERPID-0221BCA011");
+echo("<br>This program is written by Palak Messon);
 ?>
