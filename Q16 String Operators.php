371f08768a267dba06325848b832c52a9e373abf
palakmesson022 <palakmesson@gmail.com>
2025-01-30 12:13:03 +0530

    Update Q16 String Operators.php

--- Q16 String Operators.php
+++ Q16 String Operators.php
@@ -7,5 +7,5 @@
     $x .= $y;
     echo $x; // Outputs: Hello World!
 
-    echo("<br>This program is written by Prabhav Nasa<br>ERPID-0221BCA011");
+    echo("<br>This program is written by Palak Messon");
 ?>
