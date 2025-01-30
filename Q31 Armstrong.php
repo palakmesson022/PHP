b2527e44f3221ab8401c23af36bd843b22008138
palakmesson022 <palakmesson@gmail.com>
2025-01-30 12:16:35 +0530

    Update Q31 Armstrong.php

--- Q31 Armstrong.php
+++ Q31 Armstrong.php
@@ -24,5 +24,5 @@ if (isArmstrong($number)) {
     echo "$number is not an Armstrong number.";
 }
 
-echo("<br>This program is written by Prabhav Nasa<br>ERPID-0221BCA011");
+echo("<br>This program is written by Palak Messon");
 ?>
