19893cdf387a466c537e498825ba144c7bc66504
palakmesson022 <palakmesson@gmail.com>
2025-01-30 12:13:42 +0530

    Update Q19 IF Statement.php

--- Q19 IF Statement.php
+++ Q19 IF Statement.php
@@ -6,5 +6,5 @@
     {
     	echo "Out of $a and $b, $a is smaller.." ;
     }
-    echo("<br>This program is written by Prabhav Nasa<br>ERPID-0221BCA011");
+    echo("<br>This program is written by Palak Messon");
 ?>
